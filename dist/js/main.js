815867a1bd91bb099c6cf8795869210089685c33
Aaron P. Asuncion <aaronpasuncion@gmail.com>
2019-07-06 01:26:43 -0400

    improved responsiveness of images and overall site

--- dist/js/main.js
+++ dist/js/main.js
@@ -3,10 +3,12 @@ $(document).ready(function() {
     // $("#menu").css("opacity", "0");
     $("#lgMenu").addClass("enter");
     $("body").css("overflow", "hidden");
+    $("#lgMenu").css("width", "400px");
   });
   $("#exit").on("click", function() {
     $("#lgMenu").removeClass("enter");
     $("#menu").css("opacity", "1");
     $("body").css("overflow", "auto");
+    $("#lgMenu").css("width", "0px");
   });
 });
